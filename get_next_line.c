9c134c9213cb2ed4b6dd0d0f4c13efa9b654de0d
jusdal <justin.m.daly@gmail.com>
2023-03-03 23:23:29 +0700

    update gnl function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,40 @@
 /*   By: jdaly <jdaly@student.42bangkok.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 22:28:45 by jdaly             #+#    #+#             */
-/*   Updated: 2023/03/03 22:28:46 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/03/03 23:23:15 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int fn_checknl(char *str)
+{
+	int i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return 0;
+}
+
+char *get_next_line(int fd)
+{
+	static char *stash;
+	char *buf;
+	char *line;
+	int read;
+
+	while (read(fd, buf, n) != 0)
+	{
+		read(fd, buf, n);
+		stash = buff;
+		if (fn_checknl(stash))
+		{
+
+		}
+	}
+}
