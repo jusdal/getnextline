b8efe80045334d6fca683cf4740f5f77c132f4ca
Justin Daly <jdaly@z2t5c4.42bangkok.com>
2023-03-04 13:36:06 +0700

    add to main function

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: jdaly <jdaly@student.42bangkok.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 22:28:45 by jdaly             #+#    #+#             */
-/*   Updated: 2023/03/03 23:23:15 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/03/04 13:34:05 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int fn_checknl(char *str)
+int	fn_checknl(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (!str)
@@ -23,23 +23,55 @@ int fn_checknl(char *str)
 			return (1);
 		i++;
 	}
-	return 0;
+	return (0);
 }
 
-char *get_next_line(int fd)
+size_t	ft_strlen(const char *s)
 {
-	static char *stash;
-	char *buf;
-	char *line;
-	int read;
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len;
+	size_t	i;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (0);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	ft_strlcpy(str, s1, len + 1);
+	ft_strlcat(str, s2, len + 1);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*buf;
+	char		*line;
+	int			read;
 
 	while (read(fd, buf, n) != 0)
 	{
-		read(fd, buf, n);
-		stash = buff;
-		if (fn_checknl(stash))
+		if (!fn_checknl(buf))
 		{
-
+			stash = ft_strjoin(stash, buff);
+		}
+		else 
+		{
+			//copy/join buf & stash
 		}
 	}
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,16 @@
 /*   By: jdaly <jdaly@student.42bangkok.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 00:55:49 by jdaly             #+#    #+#             */
-/*   Updated: 2023/03/03 22:25:41 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/03/04 11:55:03 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
-#include <stdlib.h>
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-char *get_next_line(int fd);
+# include <unistd.h>
+# include <stdlib.h>
 
+char	*get_next_line(int fd);
+
+#endif
