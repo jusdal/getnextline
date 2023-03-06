95cfbd5bbc7e48b44ad5e6df0c0059b5232a7b9e
Justin Daly <jdaly@z3t5c1.42bangkok.com>
2023-03-07 04:14:37 +0700

    add additiional function templates

--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,26 @@
 /*   By: jdaly <jdaly@student.42bangkok.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 00:55:49 by jdaly             #+#    #+#             */
-/*   Updated: 2023/03/04 18:30:32 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/03/06 21:12:13 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 5
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
 
 
 char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int	fn_checknl(char *str);
+int	fn_splitlength(char *str);
 
 #endif
