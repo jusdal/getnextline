7cf36e4ec20ac6b72cd93ac473ac5ff09d14d719
justin daly <justin.m.daly@gmail.com>
2023-03-11 03:06:25 +0700

    remove unused functions from header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 00:55:49 by jdaly             #+#    #+#             */
-/*   Updated: 2023/03/10 19:02:39 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/03/11 03:06:24 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 10
+//# define BUFFER_SIZE 5
 
 # include <unistd.h>
 # include <stddef.h>
@@ -22,11 +22,9 @@
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s1);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
 int	fn_checknl(char *str);
 int	fn_splitlength(char *str);
 
